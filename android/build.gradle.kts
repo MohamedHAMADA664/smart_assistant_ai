5fd625f1ece2a97df292dc4724731175c30968ea
MohamedHAMADA664 <100782204+MohamedHAMADA664@users.noreply.github.com>
2026-03-19 05:14:44 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,32 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        val androidExtension = extensions.findByName("android")
+        if (androidExtension != null) {
+            val namespaceMethod = androidExtension.javaClass.methods.find {
+                it.name == "getNamespace" && it.parameterCount == 0
+            }
+            val currentNamespace = namespaceMethod?.invoke(androidExtension) as? String
+
+            if (currentNamespace.isNullOrBlank()) {
+                val setNamespaceMethod = androidExtension.javaClass.methods.find {
+                    it.name == "setNamespace" && it.parameterCount == 1
+                }
+
+                setNamespaceMethod?.invoke(
+                    androidExtension,
+                    "com.fix.${project.name.replace("-", "_")}",
+                )
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
