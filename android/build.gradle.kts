b7924582ecb131b979d6339c9460704aa05000cd
MohamedHAMADA664 <100782204+MohamedHAMADA664@users.noreply.github.com>
2026-03-19 05:25:27 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,23 +17,20 @@ subprojects {
 }
 
 subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-subprojects {
-    afterEvaluate {
+    pluginManager.withPlugin("com.android.library") {
         val androidExtension = extensions.findByName("android")
         if (androidExtension != null) {
-            val namespaceMethod = androidExtension.javaClass.methods.find {
+            val getNamespaceMethod = androidExtension.javaClass.methods.find {
                 it.name == "getNamespace" && it.parameterCount == 0
             }
-            val currentNamespace = namespaceMethod?.invoke(androidExtension) as? String
+            val setNamespaceMethod = androidExtension.javaClass.methods.find {
+                it.name == "setNamespace" && it.parameterCount == 1
+            }
+
+            val currentNamespace =
+                getNamespaceMethod?.invoke(androidExtension) as? String
 
             if (currentNamespace.isNullOrBlank()) {
-                val setNamespaceMethod = androidExtension.javaClass.methods.find {
-                    it.name == "setNamespace" && it.parameterCount == 1
-                }
-
                 setNamespaceMethod?.invoke(
                     androidExtension,
                     "com.fix.${project.name.replace("-", "_")}",
